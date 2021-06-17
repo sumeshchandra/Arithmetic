796cba14d637c69737952dc86e9be122093b2f8e
Sumesh chandra jena <sumeshjena.co@gmail.com>
2021-06-17 23:31:21 +0530

    UC-4-Arithmetic-Operation-3

--- arithmetic.sh
+++ arithmetic.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#! /bin/bash -x
 
 echo "Sorting Arithmetic Computation Problem"
 
@@ -6,6 +6,6 @@ read -p "Enter a:" a
 read -p "Enter b:" b
 read -p "Enter c:" c
 
-compute1=$(( $a + $b * $c ))
+compute2=$(( $a * $b + $c ))
 
-echo " compute1:$compute1 "
+echo " compute2:$compute2 "
